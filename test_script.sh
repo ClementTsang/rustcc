4f1d8bf67ffa80def3f3a41b94a87d6650ee4d4a
ClementTsang <clementjhtsang@gmail.com>
2018-12-24 19:20:01 -0500

    Added testing script and examples.

--- test_script.sh
+++ test_script.sh
@@ -8,52 +8,46 @@ pass () {
     echo "Passed test ${1}"
 }
 
+count=1
 run_test() {
     cargo run ${1} &> /dev/null
-    #cargo run ${1} 
-    if [ ${3} -eq 0 ]; then
+    if [ ${2} -eq 0 ]; then
         if [ "$?" -eq 0 ]; then
-            pass ${2}
+            pass ${count}
         else 
-            fail ${2}
+            fail ${count}
         fi
     else
         if [ "$?" -ne 0 ]; then
-            pass ${2}
+            pass ${count}
         else 
-            fail ${2}
+            fail ${count}
         fi
     fi
+
+    count=$((count + 1))
 }
 
-count=1
 
 echo "Test ${count}: Basic return .c files."
-run_test "./examples/ret100.c" ${count} 0
-count=$((count + 1))
+run_test "./examples/ret100.c" 0
 
 echo "Test ${count}: Changing return value."
-run_test "./examples/ret42.c" ${count} 0
-count=$((count + 1))
+run_test "./examples/ret42.c" 0
 
 echo "Test ${count}: Missing return value."
-run_test "./examples/no_ret.c" ${count} 1
-count=$((count + 1))
+run_test "./examples/no_ret.c" 1
 
 echo "Test ${count}: Missing identitfier."
-run_test "./examples/no_func_name.c" ${count} 1
-count=$((count + 1))
+run_test "./examples/no_func_name.c" 1
 
 echo "Test ${count}: Missing semicolon."
-run_test "./examples/no_semi.c" ${count} 1
-count=$((count + 1))
+run_test "./examples/no_semi.c" 1
 
 echo "Test ${count}: Missing parameter brackets."
-run_test "./examples/no_bracket.c" ${count} 1
-count=$((count + 1))
+run_test "./examples/no_bracket.c" 1
 
 echo "Test ${count}: Missing braces."
-run_test "./examples/no_brace.c" ${count} 1 
-count=$((count + 1))
+run_test "./examples/no_brace.c" 1 
 
 
